38acc6768e27994a01312907bc118e5c1e2d2e3c
LucasCieczinski <lucas.peres@sptech.school>
2026-04-23 19:53:32 -0300

    fix: AGORA VAI

--- src/main/java/school/sptech/config/DatabaseConfig.java
+++ src/main/java/school/sptech/config/DatabaseConfig.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
     // Usamos System.getenv para pegar os dados do Docker/EC2
-    private static final String URL = System.getenv().getOrDefault("DB_URL", "jdbc:postgresql://localhost:5432/TesteEtl");
-    private static final String USER = System.getenv().getOrDefault("DB_USER", "postgres");
-    private static final String PASSWORD = System.getenv().getOrDefault("DB_PASSWORD", "102030");
+    private static final String URL = System.getenv().getOrDefault("DB_URL", "jdbc:postgresql://postgres-hyper-container:5432/ConstrucaoBancoDeDados");
+    private static final String USER = System.getenv().getOrDefault("DB_USER", "hyper");
+    private static final String PASSWORD = System.getenv().getOrDefault("DB_PASSWORD", "1234");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
