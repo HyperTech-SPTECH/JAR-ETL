e4720efacf42711417eba000d150a551fd60b573
LucasCieczinski <lucas.peres@sptech.school>
2026-05-22 14:55:49 -0300

    feat: adicionada Exception para leitura de arquivos

---
+++ src/main/java/school/sptech/exception/ArquivoS3Exception.java
@@ -0,0 +1,12 @@
+package school.sptech.exception;
+
+public class ArquivoS3Exception extends RuntimeException {
+
+    public ArquivoS3Exception(String message) {
+        super(message);
+    }
+
+    public ArquivoS3Exception(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/school/sptech/service/S3Service.java
+++ src/main/java/school/sptech/service/S3Service.java
@@ -1,19 +1,38 @@
 package school.sptech.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import school.sptech.config.S3Config;
+import school.sptech.exception.ArquivoS3Exception;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import school.sptech.config.S3Config;
 import java.io.InputStream;
 
 public class S3Service {
+
+    private static final Logger log = LogManager.getLogger(S3Service.class);
     private final S3Client s3 = S3Config.getS3Client();
     private final String bucketName = "hyperbucket2026";
 
     public InputStream obterArquivo(String chaveS3) {
-        GetObjectRequest request = GetObjectRequest.builder()
-                .bucket(bucketName)
-                .key(chaveS3)
-                .build();
-        return s3.getObject(request);
+
+        try {
+            GetObjectRequest request = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(chaveS3)
+                    .build();
+
+            log.info("Buscando arquivo {} no bucket {}", chaveS3, bucketName);
+
+            return s3.getObject(request);
+
+        } catch (Exception e) {
+
+            log.error("Erro ao obter arquivo do S3", e);
+            throw new ArquivoS3Exception(
+                    "Erro ao obter arquivo do bucket S3",
+                    e
+            );
+        }
     }
 }
